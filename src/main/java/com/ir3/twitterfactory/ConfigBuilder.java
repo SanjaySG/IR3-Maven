4203009e8e560925494ca6562f3e5a3f5ad6692c
SanjaySG <sanjay4victory@gmail.com>
2016-01-04 00:35:22 -0500

    Update ConfigBuilder.java

--- src/main/java/com/ir3/twitterfactory/ConfigBuilder.java
+++ src/main/java/com/ir3/twitterfactory/ConfigBuilder.java
@@ -4,10 +4,10 @@ import twitter4j.conf.ConfigurationBuilder;
 
 public class ConfigBuilder {
 
-	private static final String OAuth_Consumer_Key = "REEAsqo0J7JtoASyohNkzM0o0";
-	private static final String OAuth_Consumer_Secret = "uO0Q2T0fDXtCBOrPHbi2iLtoXAy6DVEQXseQlFx80fh2ib8VTq";
-	private static final String OAuth_Access_Token = "70451816-APA6oKiGRw3rH14eKYj1VB7qMxQ2H6z8vyFdwjCxB";
-	private static final String OAuth_Access_Secret = "XLKJQfQ245RpXcWXfoZUDuqn2YgjLg1hVmKRw9gjVuazd";
+	private static final String OAuth_Consumer_Key = <OAuth_Consumer_Key>;
+	private static final String OAuth_Consumer_Secret = <OAuth_Consumer_Secret>;
+	private static final String OAuth_Access_Token = <OAuth_Access_Token>;
+	private static final String OAuth_Access_Secret = <OAuth_Access_Secret>;
 	private static ConfigurationBuilder conf;
 
 	protected static final ConfigurationBuilder getConfig() {
